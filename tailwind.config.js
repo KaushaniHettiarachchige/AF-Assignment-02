83bf4acc34014a34f9bc540f7374b28aeba8e461
Mona Lisa <kaushanihettiarachchige@gmail.com>
2025-04-10 17:25:21 +0530

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /**@type {import('tailwindcss').config} */
 module.exports = {
-    content: [
-        "./src/**/*.{html,js,jsx,ts,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
+    content: [ "./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: { extend: {} },
     plugins: [],
 }
