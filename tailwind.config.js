1af8c1d196cb4abbd278da715e26b0aca7267016
Mona Lisa <kaushanihettiarachchige@gmail.com>
2025-04-10 22:28:38 +0530

    added countries API

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**@type {import('tailwindcss').config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
