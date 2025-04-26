8c0f9e90cecfafd4a7fe911e617e7f398f355f50
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-04-26 19:54:47 +0530

    Changes done in Filter.js file

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Filter ({ setRegion }) {
     return (
-        <div className="mb-4">
+        <div className="mb-6">
             <select
-            className="w-full px-4 py-2 border border-gray-300 rounded"
+            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
             onChange={(e) => setRegion(e.target.value)}
             >
                 <option value="">Filter by Region</option>
