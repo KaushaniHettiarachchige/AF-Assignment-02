dc5f7b0da30cf3922c1845ef79c533e497ca03df
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-04-27 00:52:07 +0530

    Add API endpoints to contriesApi.js file

--- src/api/countriesApi.js
+++ src/api/countriesApi.js
@@ -2,32 +2,7 @@ import axios from 'axios';
 
 const baseURL = 'https://restcountries.com/v3.1/';
 
-export const getAllCountries = async () => {
-  try {
-    const response = await axios.get(`${baseURL}all`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching countries', error);
-    return [];
-  }
-};
-
-export const getCountryByName = async (name) => {
-  try {
-    const response = await axios.get(`${baseURL}name/${name}`);
-    return response.data[0];
-  } catch (error) {
-    console.error('Error fetching country by name', error);
-    return null;
-  }
-};
-
-export const getCountriesByRegion = async (region) => {
-  try {
-    const response = await axios.get(`${baseURL}region/${region}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching countries by region', error);
-    return [];
-  }
-};
+export const getAllCountries = () => axios.get(`${BASE_URL}/all`);
+export const serachCountryByName = (name) => axios.get(`${BASE_URL}/name/${name}`);
+export const getCountriesByRegion = (region) => axios.get(`${BASE_URL}/region/${region}`);
+export const getCountryByCode = (code) => axios.get(`${BASE_URL}/alpha/${code}`);
