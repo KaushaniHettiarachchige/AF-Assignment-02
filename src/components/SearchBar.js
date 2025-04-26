22eef676ba5da1b8234f9010dd2aa332543a0611
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-04-26 19:55:53 +0530

    Changes done in SearchBar.js file

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function SearchBar({ setSearch }) {
     return (
-        <div className="mb-4">
+        <div className="mb-6">
             <input 
             type="text"
             placeholder="Search by country name"
-            className="w-full px-4 py-2 border border-gray-300 rounded"
+            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
             onChange={(e) => setSearch(e.target.value)} 
             />
         </div>
