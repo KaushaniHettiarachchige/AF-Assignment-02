d6f0246d21d8023efeb87258fc7f4db73178357a
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-05-02 01:25:35 +0530

    Changes done in api.js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,43 @@
-// import axios from 'axios';
-
-// const baseURL = 'https://restcountries.com/v3.1/';
-
-// export const getAllCountries = () => axios.get(`${baseURL}/all`);
-// export const searchCountryByName = (name) => axios.get(`${baseURL}/name/${name}`);
-// export const getCountriesByRegion = (region) => axios.get(`${baseURL}/region/${region}`);
-// export const getCountryByCode = (code) => axios.get(`${baseURL}/alpha/${code}`);
-
 import axios from 'axios';
 
-const BASE_URL = 'https://restcountries.com/v3.1/';
+const API_BASE_URL = 'https://restcountries.com/v3.1';
 
-export const fetchAllCountries = async () => {
-  const res =  await fetch(`${BASE_URL}/all`);
-  return res.json();
+export const getAllCountries = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/all`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching all countries:', error);
+    throw error;
+  }
 };
 
-export const fetchByName = async (name) => {
-  const res = await fetch(`${BASE_URL}/name/${name}`);
-  return res.json();
+export const getCountryByName = async (name) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/name/${name}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching country by name:', error);
+    throw error;
+  }
 };
 
-export const fetchByRegion = async (region) => {
-  const res = await fetch(`${BASE_URL}/region/${region}`);
-  return res.json();
+export const getCountriesByRegion = async (region) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/region/${region}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching countries by region:', error);
+    throw error;
+  }
 };
 
-export const fetchByAlphaCode = async (code) => {
-  const res = await fetch(`${BASE_URL}/alpha/${code}`);
-  return res.json();
-};
+export const getCountryByCode = async (code) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/alpha/${code}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching country by code:', error);
+    throw error;
+  }
+}; 
