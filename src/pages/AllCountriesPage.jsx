e8fec346ea3480ab4624818220a87ca2ff44029f
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2025-05-03 12:36:29 +0530

    Changes done in AllCountriesPage.jsx file

--- src/pages/AllCountriesPage.jsx
+++ src/pages/AllCountriesPage.jsx
@@ -21,8 +21,14 @@ const AllCountriesPage = () => {
     fetchCountries();
   }, []);
 
-  if (loading) return <div className="flex justify-center items-center min-h-screen"><div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div></div>;
-  if (error) return <div className="flex justify-center items-center min-h-screen"><p className="text-red-500 text-xl">{error}</p></div>;
+  if (loading) return 
+    <div className="flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+    </div>;
+  if (error) return 
+    <div className="flex justify-center items-center min-h-screen">
+        <p className="text-red-500 text-xl">{error}</p>
+    </div>;
 
   return (
     <div className="container mx-auto px-4 py-8">
